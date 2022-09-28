8631e5c47f6522e36596967910a09f4d1390b96e
Azoth999 <yanispeyrat@gmail.com>
2022-09-28 17:54:29 +0200

    Ajout chef
    Il manque toujours le village mais me souvient plus de ce qu'il fait.

---
+++ src/personnages/Chef.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package personnages;
+
+/**
+ * @author Yanis
+ *
+ */
+public class Chef {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	private Village village;
+	
+	public Chef(String nom, int force, int effetPotion, Village village) {
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+		this.village = village;
+	}
+	
+	public String getNom() {
+	return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le chef " + nom + " du village " + village.getNom() + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
+			romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+}
