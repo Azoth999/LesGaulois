3c53f72ef66db2c012f684f450828a4b8661fe77
Azoth999 <yanispeyrat@gmail.com>
2022-10-05 14:44:52 +0200

    Update Romain.java
    Ajout d'asserts pour savoir si la force est initialisée positive et si elle diminue quand il prend un coup.

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -14,6 +14,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert force>0;
 	}
 
 	public String getNom() {
@@ -29,18 +30,21 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert force>0;
+		int forceInit = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("AÃ¯e");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert force<forceInit;
 	}
 	
 	public static void main(String[] args) {
-		Romain romain = new Romain("Romain", 5);
+		Romain romain = new Romain("Minus", 6);
 		System.out.println(romain.prendreParole());
-		romain.parler("Je vais te détruire!");
+		romain.parler("Je vais te dï¿½truire!");
 		romain.recevoirCoup(4);
 		romain.recevoirCoup(5);
 	}
