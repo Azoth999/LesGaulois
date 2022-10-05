3fda5813f5bbd49da69efc0d0988b8eedb2bd6c3
Azoth999 <yanispeyrat@gmail.com>
2022-10-05 13:53:37 +0200

    Update Village.java

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -9,17 +9,19 @@ package personnages;
  */
 public class Village {
 	public String nom;
+	public int nbVillageois = 0;
 	public Gaulois[] population;
 	public Chef chef;
-	public Village(String nom, Chef chef, Gaulois[] population) {
+	public Village(String nom,int nbVillageoisMaximum, Chef chef) {
 		this.nom = nom;
-		this.population = population;
+		this.population =population[nbVillageoisMaximum];
 		this.chef = chef;
 	}
 	
 	public String getNom() {
 		return nom;
 	}
+	
 	public Chef getChef() {
 		return chef;
 	}
